7c29db351da098ad9e9c4419cabe4ec728f547a1
Kevin Wang <kwangsan@gmail.com>
2020-06-01 08:50:57 -0400

    add emojis to deploy script

--- async_await_fn/deploy.sh
+++ async_await_fn/deploy.sh
@@ -2,10 +2,20 @@
 set -e
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-cargo build --release --target x86_64-unknown-linux-musl
+LAMBDA_NAME="test-rust"
 
+echo 🏗 building \`$LAMBDA_NAME\`
+cargo build --release --target x86_64-unknown-linux-musl
+echo ✅ finished building
+
+echo 🤐 zipping \`$LAMBDA_NAME\`
 zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
+echo ✅ finished zipping
+
+echo ⬆️ uploading \`$LAMBDA_NAME\` to AWS ☁️ 
 
 aws lambda update-function-code \
-  --function-name test-rust \
+  --function-name $LAMBDA_NAME \
   --zip-file fileb://rust.zip
+
+echo 🎉 Successfully uploaded to \`$LAMBDA_NAME\`
